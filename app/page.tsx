198c7beb33f3bd0ebccdbfa00a9a63a14646b324
tinjet_11 <tinjet1016@gmail.com>
2023-06-20 10:40:48 +0800

    fix bugs

--- app/page.tsx
+++ app/page.tsx
@@ -5,26 +5,7 @@ import NavHorizontal from "@/components/NavHorizontal";
 import NavVertical from "@/components/NavVertical";
 import { useEffect, useState } from "react";
 
-export default function Home({ }: { users: userProps[] }) {
-  const [userList, setUserList] = useState<userProps[]>([]);
-
-  useEffect(() => {
-    getUsers()
-      .then((data) => setUserList(data))
-      .catch((error) => console.error(error));
-  }, []);
-
-  async function getUsers() {
-    try {
-      const res = await fetch("https://jsonplaceholder.typicode.com/users");
-      if (!res.ok) {
-        throw new Error("Failed to fetch data");
-      }
-      return res.json();
-    } catch (error) {
-      throw new Error("Failed to fetch data");
-    }
-  }
+export default function Home() {
 
   return (
     <>
@@ -38,10 +19,4 @@ export default function Home({ }: { users: userProps[] }) {
         </div>
     </>
   );
-}
-
-export interface userProps {
-  id: number;
-  name: string;
-  website: string;
 }
