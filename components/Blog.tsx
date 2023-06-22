2decb0556c3e6b46d40a2b224e6303c7eec997a5
tinjet_11 <tinjet1016@gmail.com>
2023-06-22 15:02:41 +0800

    infinite scrolling, load more

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -1,80 +1,125 @@
-import Image from 'next/image'
-import React from 'react'
-import Blog_card from './Blog_card'
+"use client"
+
+import { useInfiniteQuery, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React, { useEffect, useRef } from 'react';
+import Blog_card from './Blog_card';
+import { blogData } from '@/constant';
+import { useIntersection } from '@mantine/hooks';
+
+const posts = blogData;
+
+const fetchPost = async (page: number) => {
+    // this below code is to simulate the database fetching
+    //await new Promise((resolve) => setTimeout(resolve, 1000));
+    return posts.slice((page - 1) * 3, page * 3);
+};
+
+const App = () => {
+    const { data, fetchNextPage, isFetchingNextPage } = useInfiniteQuery(
+        ['query'],
+        async ({ pageParam = 1 }) => {
+            const response = await fetchPost(pageParam);
+            return response;
+        },
+        {
+            getNextPageParam: (_, pages) => {
+                return pages.length + 1;
+            },
+            initialData: {
+                pages: [posts.slice(0, 3)],
+                pageParams: [1],
+            },
+        }
+    );
+
+    const lastPostRef = useRef<HTMLElement>(null);
+
+    const { ref, entry } = useIntersection({
+        root: lastPostRef.current,
+        threshold: 1,
+    });
+
+    useEffect(() => {
+        if (entry?.isIntersecting) fetchNextPage();
+    }, [entry]);
+
+    const _posts = data?.pages.flatMap((page) => page);
+    return (
+        <div className="container">
+            <div className="flex flex-col m-2 bg-white rounded-lg shadow-lg p-4">
+                <h1 className="text-center font-bold text-4xl mb-3">Blog</h1>
+                <hr />
+                <div className="flex flex-col gap-3 md:grid md:grid-cols-2 lg:grid-cols-3 mt-10">
+                    {_posts?.map((page, i) => {
+                        const pageArray = Array.isArray(page) ? page : [page];
+                        if (i === _posts.length - 1)
+                            return (pageArray.map((post, index) => (
+                                <div ref={ref}>
+                                    <Blog_card
+                                        key={index}
+                                        title={post.title}
+                                        subtitle={post.subtitle}
+                                        date={post.date}
+                                        image={post.image}
+                                    />
+                                </div>
+
+                            )))
+
+                        return pageArray.map((post, index) => (
+                            <Blog_card
+                                key={index}
+                                title={post.title}
+                                subtitle={post.subtitle}
+                                date={post.date}
+                                image={post.image}
+                            />
+                        ))
+                    })}
+
+
+
+                    {/* using button to expand */}
+                    {/*   {
+                        data?.pages.map((page,i)=>(
+                            page.map((post, index) => (
+                                <Blog_card
+                                    key={index}
+                                    title={post.title}
+                                    subtitle={post.subtitle}
+                                    date={post.date}
+                                    image={post.image}
+                                />
+                            ))
+                        ))
+                    }
+ */}
+                </div>
+
+                {/*                {  <button
+          onClick={() => fetchNextPage()}
+          disabled={isFetchingNextPage}
+          className="mt-3 w-32 self-end bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded"
+        >
+          {isFetchingNextPage
+            ? 'Loading More'
+            : (data?.pages.length ?? 0) < 10
+            ? 'Load More'
+            : 'Nothing more to load'}
+        </button> } */}
+            </div>
+        </div>
+    );
+};
+
+const queryClient = new QueryClient();
 
 const Blog = () => {
     return (
-        <div className='container'>
-        <div className='flex flex-col m-2 bg-white rounded-lg shadow-lg p-4'>
-            <h1 className='text-center font-bold text-4xl m-3'>Blog</h1>
-            <hr />
-         
-            <div className='flex flex-col sm:flex-row gap-3 mt-4 p-2 justify-evenly'>
-                <Blog_card
-                    title={'CS induction night'}
-                    subtitle={'Having fun'}
-                    date={'16 October 2023'}
-                    image={'/nav/logo.svg'}
-                />
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
+    );
+};
 
-                <Blog_card
-                    title={'CS induction night'}
-                    subtitle={'Having fun'}
-                    date={'16 October 2023'}
-                    image={'/nav/logo.svg'}
-                />
-                 <Blog_card 
-             title={'CS induction night'} 
-             subtitle={'Having fun'} 
-             date={'16 October 2023'} 
-             image={'/nav/logo.svg'}             
-             />
-            </div>
-            <div className='flex flex-col sm:flex-row gap-4 mt-4 p-2 justify-evenly'>
-                <Blog_card
-                    title={'CS induction night'}
-                    subtitle={'Having fun'}
-                    date={'16 October 2023'}
-                    image={'/nav/logo.svg'}
-                />
-
-                <Blog_card
-                    title={'CS induction night'}
-                    subtitle={'Having fun'}
-                    date={'16 October 2023'}
-                    image={'/nav/logo.svg'}
-                />
-                 <Blog_card 
-             title={'CS induction night'} 
-             subtitle={'Having fun'} 
-             date={'16 October 2023'} 
-             image={'/nav/logo.svg'}             
-             />
-            </div>
-            <div className='flex flex-col  sm:flex-row gap-4 mt-4 p-2 justify-evenly'>
-                <Blog_card
-                    title={'CS induction night'}
-                    subtitle={'Having fun'}
-                    date={'16 October 2023'}
-                    image={'/nav/logo.svg'}
-                />
-
-                <Blog_card
-                    title={'CS induction night'}
-                    subtitle={'Having fun'}
-                    date={'16 October 2023'}
-                    image={'/nav/logo.svg'}
-                />
-                 <Blog_card 
-             title={'CS induction night'} 
-             subtitle={'Having fun'} 
-             date={'16 October 2023'} 
-             image={'/nav/logo.svg'}             
-             />
-            </div>
-        </div>
-        </div>
-    )
-}
-
-export default Blog
+export default Blog;
---
+++ constant/index.ts
@@ -0,0 +1,189 @@
+export const blogData = [
+    {
+      "title": "CS Induction Night",
+      "subtitle": "Having fun",
+      "date": "October 16, 2023",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Exploring React Hooks",
+      "subtitle": "A deep dive into useState and useEffect",
+      "date": "September 22, 2023",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "The Power of CSS Grid",
+      "subtitle": "Creating responsive layouts with ease",
+      "date": "August 7, 2023",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Introduction to JavaScript",
+      "subtitle": "Getting started with the fundamentals",
+      "date": "July 14, 2023",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Mastering HTML5 Canvas",
+      "subtitle": "Creating interactive graphics and animations",
+      "date": "May 5, 2023",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Deep Learning and Neural Networks",
+      "subtitle": "Understanding the fundamentals",
+      "date": "April 2, 2023",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Building RESTful APIs with Node.js",
+      "subtitle": "A practical guide to server-side development",
+      "date": "March 19, 2023",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Data Visualization with D3.js",
+      "subtitle": "Creating stunning visual representations",
+      "date": "February 8, 2023",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Responsive Web Design Techniques",
+      "subtitle": "Optimizing websites for different devices",
+      "date": "January 11, 2023",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Introduction to Python Programming",
+      "subtitle": "Learn Python from scratch",
+      "date": "December 3, 2022",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Getting Started with Git and GitHub",
+      "subtitle": "Version control made easy",
+      "date": "November 16, 2022",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "The Basics of UX Design",
+      "subtitle": "Creating user-centered experiences",
+      "date": "October 8, 2022",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Introduction to Data Science",
+      "subtitle": "Exploring the world of data analysis",
+      "date": "September 20, 2022",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "JavaScript Best Practices",
+      "subtitle": "Writing clean and maintainable code",
+      "date": "August 9, 2022",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Mastering CSS Flexbox",
+      "subtitle": "Efficient layout design techniques",
+      "date": "July 17, 2022",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Introduction to React Native",
+      "subtitle": "Building mobile apps with JavaScript",
+      "date": "June 25, 2022",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Securing Web Applications",
+      "subtitle": "Best practices for web application security",
+      "date": "May 19, 2022",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Advanced CSS Techniques",
+      "subtitle": "Mastering CSS selectors and animations",
+      "date": "April 8, 2022",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Introduction to Machine Learning",
+      "subtitle": "An overview of ML algorithms and concepts",
+      "date": "March 14, 2022",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Building Web Applications with Angular",
+      "subtitle": "A comprehensive guide to Angular development",
+      "date": "February 2, 2022",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "The Art of Debugging",
+      "subtitle": "Effective strategies for finding and fixing bugs",
+      "date": "January 7, 2022",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Introduction to TypeScript",
+      "subtitle": "Type-safe JavaScript development",
+      "date": "December 9, 2021",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Responsive Email Design",
+      "subtitle": "Creating visually appealing email templates",
+      "date": "November 18, 2021",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Data Structures and Algorithms in Python",
+      "subtitle": "Essential concepts for efficient programming",
+      "date": "October 27, 2021",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Optimizing Website Performance",
+      "subtitle": "Improving speed and user experience",
+      "date": "September 15, 2021",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Introduction to Cybersecurity",
+      "subtitle": "Protecting digital assets and privacy",
+      "date": "August 24, 2021",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Web Accessibility Guidelines",
+      "subtitle": "Designing for all users",
+      "date": "July 31, 2021",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Introduction to Vue.js",
+      "subtitle": "Building interactive web interfaces",
+      "date": "June 28, 2021",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Secure Coding Practices",
+      "subtitle": "Writing secure and resilient code",
+      "date": "May 22, 2021",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Mastering React Hooks",
+      "subtitle": "Advanced state management with hooks",
+      "date": "April 19, 2021",
+      "image": "/nav/logo.svg"
+    },
+    {
+      "title": "Responsive Design for Mobile First",
+      "subtitle": "Designing for small screens",
+      "date": "March 13, 2021",
+      "image": "/nav/logo.svg"
+    }
+  ]
+  
