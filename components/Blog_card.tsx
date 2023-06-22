c7b12f321f23d3432a25220413d925967adf2750
tinjet_11 <tinjet1016@gmail.com>
2023-06-22 16:13:51 +0800

    style redesign

--- components/Blog_card.tsx
+++ components/Blog_card.tsx
@@ -4,26 +4,23 @@ import React from 'react';
 
 const BlogCard = ({ title, subtitle, date, image }: blogCardProps) => {
   return (
-    <div className="bg-white rounded-lg shadow-md p-4 hover:bg-indigo-300">
+    <div className="bg-gray-100 rounded-lg shadow-lg p-6 hover:bg-indigo-100 flex flex-col">
       <div className="flex justify-center">
         <Image
           src={`${image}`}
           alt="blog cover image"
           width={300}
-          height={300}
-          className="object-contain drop-shadow-2xl"
+          height={200}
+          className="object-cover rounded-lg"
         />
       </div>
 
-      <div className="text-start mt-4">
-        <p className="text-xl font-bold text-gray-800">{title}</p>
+      <div className="mt-4">
+        <h3 className="text-xl font-bold text-gray-800">{title}</h3>
+        <p className="text-gray-600 mt-2">{subtitle}</p>
       </div>
 
-      <div className="text-start mt-2 ml-2">
-        <p className="text-gray-600">{subtitle}</p>
-      </div>
-
-      <div className="mt-auto text-right text-xs font-light text-gray-400">
+      <div className="mt-4 text-right text-xs font-light text-gray-400">
         <p>Posted on {date}</p>
       </div>
     </div>
