a96a9f1ec5b64226c000251b09955b1034a888e9
tinjet_11 <tinjet1016@gmail.com>
2023-06-24 12:24:04 +0800

    responsive design

--- components/Events.tsx
+++ components/Events.tsx
@@ -8,7 +8,8 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
 import 'swiper/css';
 import "swiper/css/navigation";
-import { Navigation } from 'swiper';
+import "swiper/css/pagination";
+import { Pagination } from 'swiper';
 import Events_card from './Events_card';
 
 const Events = () => {
@@ -16,16 +17,22 @@ const Events = () => {
         <div className='container'>
             <div className='flex flex-col m-2 bg-white rounded-lg shadow-lg p-4'>
                 <h1 className="text-center font-bold text-4xl mb-3">Events</h1>
-                <hr/>
-            
-                <Swiper navigation={true} modules={[Navigation]} className="mySwiper mt-2">
-                    <SwiperSlide><Events_card/></SwiperSlide>
-                    <SwiperSlide><Events_card/></SwiperSlide>
-                    <SwiperSlide><Events_card/></SwiperSlide>
+                <hr />
+
+                <Swiper
+                    spaceBetween={40}
+                    pagination={{
+                        dynamicBullets: true,
+                    }}
+                    modules={[Pagination]}
+                    className="mySwiper mt-2">
+                    <SwiperSlide><Events_card /></SwiperSlide>
+                    <SwiperSlide><Events_card /></SwiperSlide>
+                    <SwiperSlide><Events_card /></SwiperSlide>
                 </Swiper>
-                
+
             </div>
- 
+
         </div>
     )
 }
--- components/Events_card.tsx
+++ components/Events_card.tsx
@@ -40,83 +40,86 @@ const Events_card = () => {
     }, []);
 
     return (
-        <>
-            <div className='flex flex-col'>
+        <div className='flex flex-col'>
+            
+             <div className="flex justify-center items-center my-4">
+                <div className="flex items-center">
+                    <span className="text-2xl md:text-3xl font-bold">{days}</span>
+                    <span className="text-gray-500 ml-1">
+                    {window.innerWidth < 768 ? `D` : `Days`}
+                    </span>
+                </div>
+                <span className="mx-1 md:mx-2 text-gray-500">:</span>
+                <div className="flex items-center">
+                    <span className="text-2xl md:text-3xl font-bold">{hours}</span>
+                    <span className="text-gray-500 ml-1">
+                    {window.innerWidth < 768 ? `H` : `Hours`}
+                    </span>
+                </div>
+                <span className="mx-1 md:mx-2 text-gray-500">:</span>
+                <div className="flex items-center">
+                    <span className="text-2xl md:text-3xl font-bold">{minutes}</span>
+                    <span className="text-gray-500 ml-1">
+                    {window.innerWidth < 768 ? `M` : `Minutes`}
+                    </span>
+                </div>
+                <span className="mx-1 md:mx-2 text-gray-500">:</span>
+                <div className="flex items-center">
+                    <span className="text-2xl md:text-3xl font-bold">{seconds}</span>
+                    <span className="text-gray-500 ml-1">
+                    {window.innerWidth < 768 ? `S` : `Seconds`}
+                    </span>
+                </div>
+            </div>  
 
-                <div className="flex justify-center items-center my-4">
-                    <div className="flex items-center">
-                        <span className="text-4xl font-bold">{days}</span>
-                        <span className="text-gray-500 ml-1">Days</span>
-                    </div>
-                    <span className="mx-2 text-gray-500">:</span>
-                    <div className="flex items-center">
-                        <span className="text-4xl font-bold">{hours}</span>
-                        <span className="text-gray-500 ml-1">Hours</span>
-                    </div>
-                    <span className="mx-2 text-gray-500">:</span>
-                    <div className="flex items-center">
-                        <span className="text-4xl font-bold">{minutes}</span>
-                        <span className="text-gray-500 ml-1">Minutes</span>
-                    </div>
-                    <span className="mx-2 text-gray-500">:</span>
-                    <div className="flex items-center">
-                        <span className="text-4xl font-bold">{seconds}</span>
-                        <span className="text-gray-500 ml-1">Seconds</span>
-                    </div>
+            <div className='flex flex-col md:flex-row p-4 justify-evenly'>
+                <div className='w-full md:w-1/2'>
+                    <Image
+                        width={500}
+                        height={200}
+                        src={'/bitcoin.svg'}
+                        alt={'Events poster'}
+                        className='object-contain'
+                    />
                 </div>
 
+                <div className='flex flex-col justify-between p-2 mt-4 md:mt-0'>
+                    <div className='items-center'>
+                        <h1 className='text-lg font-bold mb-3'>Introduction to WEB3</h1>
 
-                <div className='flex flex-row p-4 justify-evenly'>
-                    <div className='w-1/2'>
-                        <Image
-                            width={500}
-                            height={200}
-                            src={'/bitcoin.svg'}
-                            alt={'Events poster'}
-                            className='object-contain'
-                        />
+                        <div className='mb-3'>
+                            <h1 className='text-lg font-bold'>Venue:</h1>
+                            <p className='text-base'>F1A11</p>
+                        </div>
+
+                        <div className='mb-3'>
+                            <h1 className='text-lg font-bold'>Date & Time:</h1>
+                            <p className='text-base'>8th March 2023, 6pm-8pm</p>
+                        </div>
+                        <div className='mb-3'>
+                            <h1 className='text-lg font-bold mb-3'>Sessions:</h1>
+                            <ul className='list-disc ml-6'>
+                                <li>Getting to know Web3</li>
+                                <li>Introduction to blockchain</li>
+                                <li>Doing live transactions</li>
+                            </ul>
+                        </div>
+
+                        <h1 className='text-lg font-bold'>Join us for</h1>
+                        <ul className='list-disc ml-6'>
+                            <li>Free pizza</li>
+                            <li>2 scpd points</li>
+                        </ul>
                     </div>
 
-                    <div className='flex flex-col justify-between p-2'>
-                        <div className='items-center'>
-                            <h1 className='text-lg font-bold mb-3'>Introduction to WEB3</h1>
-
-                            <div className='mb-3'>
-                                <h1 className='text-lg font-bold'>Venue:</h1>
-                                <p className='text-base'>F1A11</p>
-                            </div>
-
-                            <div className='mb-3'>
-                                <h1 className='text-lg font-bold'>Date & Time:</h1>
-                                <p className='text-base'>8th March 2023, 6pm-8pm</p>
-                            </div>
-                            <div className='mb-3'>
-                                <h1 className='text-lg font-bold mb-3'>Sessions:</h1>
-                                <ul className='list-disc ml-6'>
-                                    <li>Getting to know Web3</li>
-                                    <li>Introduction to blockchain</li>
-                                    <li>Doing live transactions</li>
-                                </ul>
-                            </div>
-
-
-                            <h1 className='text-lg font-bold'>Join us for</h1>
-                            <ul className='list-disc ml-6'>
-                                <li>Free pizza</li>
-                                <li>2 scpd points</li>
-                            </ul>
-
-                        </div>
-
-                        <div className='self-end'>
-                            <button className='bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded'>
-                                Join Now
-                            </button>
-                        </div>
+                    <div className='self-end mt-4'>
+                        <button className='bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded'>
+                            Join Now
+                        </button>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
