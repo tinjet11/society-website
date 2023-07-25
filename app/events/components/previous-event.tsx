8ae3e4e70691080cb1347dda4b5f9e70b8cfe82d
tinjet_11 <tinjet1016@gmail.com>
2023-07-25 12:13:35 +0800

    sekelton added

--- app/events/components/previous-event.tsx
+++ app/events/components/previous-event.tsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react';
 import getBlogs from '@/actions/getBlogs';
 import BlogCard from './Blog_card';
 import { usePathname } from 'next/navigation';
+import { Skeleton } from '@/components/ui/skeleton';
 
 
 export const revalidate = 0;
@@ -12,13 +13,16 @@ export const revalidate = 0;
 const PreviousEvent = () => {
 
     const [blog, setBlog] = useState<BlogType[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
     const [isMounted, setIsMounted] = useState(false);
     const pathname = usePathname();
     useEffect(() => {
 
         const fetchBlogs = async () => {
+            setIsLoading(true)
             const blogs = await getBlogs();
             setBlog(blogs)
+            setIsLoading(false)
         }
 
 
@@ -37,26 +41,37 @@ const PreviousEvent = () => {
 
 
     return (
-        <div className="flex flex-col gap-3 md:grid md:grid-cols-2 mt-10">
+        <>
+            {isLoading ? (
+                <Skeleton className="w-full h-[500px] mt-4 aspect-square" />
+            ) : (
+                <>
+                    <div className="flex flex-col gap-3 md:grid md:grid-cols-2 mt-10">
 
-            {pathname === '/events' && blog.map((item) => (
-                <div className="card-bg rounded-lg shadow-2xl p-4 flex flex-col" key={item.id} id={item.id} >
-                    <BlogCard
-                        data={item}
-                    />
-                </div>
-            ))}
+                        {pathname === '/events' && blog.map((item) => (
+                            <div className="card-bg rounded-lg shadow-2xl p-4 flex flex-col" key={item.id} id={item.id} >
+                                <BlogCard
+                                    data={item}
+                                />
+                            </div>
+                        ))}
 
-            {pathname === '/' && blog.slice(0, 2).map((item) => (
-                <div className="card-bg rounded-lg shadow-2xl p-4 flex flex-col" key={item.id} id={item.id} >
-                    <BlogCard
-                        data={item}
-                    />
-                </div>
-            ))}
+                        {pathname === '/' && blog.slice(0, 2).map((item) => (
+                            <div className="card-bg rounded-lg shadow-2xl p-4 flex flex-col" key={item.id} id={item.id} >
+                                <BlogCard
+                                    data={item}
+                                />
+                            </div>
+                        ))}
 
-        </div>
+                    </div>
+                </>
+
+            )
+            }
+        </>
     )
 }
+
 
 export default PreviousEvent
